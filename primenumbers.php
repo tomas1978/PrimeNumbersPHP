d366b54f52172ac3c887731924c32478fddcb331
tomas1978 <tomas.nilsson78@gmail.com>
2021-03-18 19:03:00 +0100

    First attempt to make for loop
    My first attempt to make a for-loop to echo all integers from 2 to 99 on page

---
+++ primenumbers.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Prime numbers</title>
+	</head>
+	<body>
+		<h1>Prime numbers in an interval</h1>
+		<?php
+			for($i=2;$i<100;$i++) {
+				echo i;
+			}
+		?>
+	</body>
+</html>
