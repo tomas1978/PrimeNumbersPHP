8cce96e795230ca9f6af8c9483bca645df042458
tomas1978 <tomas.nilsson78@gmail.com>
2021-03-25 18:36:56 +0100

    More effective evaulation
    Changed the loop so that it only checks divisibility up to the square root of the number to be checked, this makes it more effective.

--- primenumbers/primenumbers.php
+++ primenumbers/primenumbers.php
@@ -6,7 +6,22 @@
 	<body>
 		<h1>Prime numbers in an interval</h1>
 		<?php
-			echo "Test";
+			function isPrime($tal) {
+				$isPrime=true;
+				for($i=2;$i<sqrt($tal);$i++) {
+					if($tal%$i==0)
+						$isPrime=false;
+				}
+				return ($isPrime);
+			}
+			
+			$first=1000;
+			$last=1100;
+			
+			for($i=$first;$i<$last;$i++) {
+				if(isPrime($i))
+					echo($i." ");
+			}
 		?>
 	</body>
 </html>
