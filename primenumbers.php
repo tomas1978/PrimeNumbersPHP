e600634ff9622911574c95dce0c60e3eee75124f
tomas1978 <tomas.nilsson78@gmail.com>
2021-04-06 11:45:48 +0200

    Added input fields
    Introduced input fields for lower and upper limit

--- primenumbers.php
+++ primenumbers.php
@@ -5,10 +5,15 @@
 	</head>
 	<body>
 		<h1>Prime numbers in an interval</h1>
+		<form>
+			Upper limit <input id="upper" />
+			Lower limit <input id="lower" />
+		</form>
+		<h2>Result</h2>
 		<?php
 			function isPrime($tal) {
 				$isPrime=true;
-				for($i=2;$i<$tal;$i++) {
+				for($i=2;$i<sqrt($tal);$i++) {
 					if($tal%$i==0)
 						$isPrime=false;
 				}
